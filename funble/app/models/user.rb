fd62e41f452626b7066dfd75b59b31ebf5c3c7a9
Markol17 <markolivierpoulin@hotmail.com>
2019-11-06 01:54:40 -0500

    cleaning

--- funble/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :username, uniqueness: true, presence: true
-
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
